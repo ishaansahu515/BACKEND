63de448e815b2922a216255115592122d2e58f72
ISHAAN SAHU <98735376+ishaan51551@users.noreply.github.com>
2023-01-14 10:17:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.t3rtmhc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
+  .set("strictQuery", false)
   .then(() => {
     app.listen(5001);
   })

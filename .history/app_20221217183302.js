c748b7c8c24494e554d28963ce4f2ba0703d19af
ishan51551 <ishaansahu515@gmail.com>
2023-01-14 11:26:20 +0530

    cors

--- .history/app_20221217183302.js
+++ .history/app_20221217183302.js
@@ -10,11 +10,6 @@ const app = express();
 
 app.use(bodyParser.json());//it tells the system that we have to use json or returns middleware that only parses JSON
 
-app.use("/",(req,res)=>{
-   res.status(200).send("Api is running");
-})
-app.use(cors);
-
 app.use('/uploads/images',express.static(path.join('uploads','images')));
 
 app.use((req, res, next) => {
@@ -52,12 +47,77 @@ app.use((error, req, res, next) => {
 
 
 mongoose
-  .set("strictQuery", false)
   .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.t3rtmhc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
   .then(() => {
-    app.listen(process.env.PORT);
+    app.listen(5001);
   })
   .catch(err => {
     console.log(err);
   });
 
+
+
+
+
+
+
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+
+// const app = express();
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.post('/user', (req, res, next) => {
+//   res.send('<h1>User: ' + req.body.username + '</h1>');
+// });
+
+// app.get('/', (req, res, next) => {
+//   res.send(
+//     '<form action="/user" method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>'
+//   );
+// });
+
+// app.listen(5000);
+
+
+//-------------------------------------------------------------
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+//   console.log('INCOMING REQUEST');
+//   console.log(req.method, req.url);
+
+//   if(req.method=='POST'){
+// let body='';
+// req.on('end',()=>{
+//     const username=body.split('=')[1];
+
+//     res.end('<h1>'+ username+'</h1>')
+// })
+// req.on('data',(chunk)=>{
+//     body+=chunk;
+// });
+//   }
+//   else{
+//   res.setHeader('Content-Type','text/html');
+//   res.end('<form method="POST"><input type="text" name="username"><button>Create users</button></form>');
+//   }
+// });
+
+// server.listen(5000);
+
+//---------------------------------------------------------------------------------
+
+// const fs= require('fs'); //new APIs.
+//  const userName='Max';
+// //  console.log(userName);
+// //   alert(userName);//cant use it
+
+//  fs.writeFile('user-data.txt','Name'+ userName,(err)=>{
+//     if(err){
+//         console.log(err);
+//         return;
+//     }
+//     console.log('WROTE-FILE');
+//  });
